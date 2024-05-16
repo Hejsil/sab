536b9b58e39fb9a2612a4a2b9388869c3439c357
Jimmi Holst Christensen <git@hejsil.anonaddy.me>
2024-05-16 09:45:01 +0200

    Update to newest version of zig

--- build.zig
+++ build.zig
@@ -4,21 +4,21 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    _ = b.addModule("sab", .{ .root_source_file = .{ .path = "src/main.zig" } });
+    _ = b.addModule("sab", .{ .root_source_file = b.path("src/main.zig") });
 
     const exe = b.addExecutable(.{
         .name = "sab",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
     exe.root_module.addAnonymousImport("clap", .{
-        .root_source_file = .{ .path = "lib/zig-clap/clap.zig" },
+        .root_source_file = b.path("lib/zig-clap/clap.zig"),
     });
 
     const test_step = b.step("test", "");
     const tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
